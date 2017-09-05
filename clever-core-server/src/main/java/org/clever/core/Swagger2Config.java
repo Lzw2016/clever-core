06e7eb73f477fb8dcdbbe168cb3949b0ca432f1e
Lzw2016 <lizhiwei1993>
2017-09-05 11:50:44 +0800

    重构

--- clever-core-server/src/main/java/org/clever/core/Swagger2Config.java
+++ clever-core-server/src/main/java/org/clever/core/Swagger2Config.java
@@ -6,7 +6,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -32,16 +31,16 @@ public class Swagger2Config {
     }
 
     private ApiInfo apiInfo() {
-        Contact contact = new Contact("name", "url", "email");
+        // Contact contact = new Contact("name", "url", "email");
         return new ApiInfoBuilder()
                 .title("clever-core服务")
-                .description("description")
-                .termsOfServiceUrl("termsOfServiceUrl")
+                // .description("description")
+                // .termsOfServiceUrl("termsOfServiceUrl")
                 .version("1.0")
-                .license("license")
-                .licenseUrl("licenseUrl")
-                .termsOfServiceUrl("termsOfServiceUrl")
-                .contact(contact)
+                // .license("license")
+                // .licenseUrl("licenseUrl")
+                // .termsOfServiceUrl("termsOfServiceUrl")
+                // .contact(contact)
                 // .extensions()
                 .build();
     }
